9ee103fd4319ad8bfb9af6ee647da42a0a690d15
Farid <mfaridn03@gmail.com>
2026-03-18 18:09:32 +1100

    feat: basic bot

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import discord
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=",", intents=intents)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+
+bot_token = os.environ.get("bot-token")
+if not bot_token:
+    raise Exception("bot token error")
+
+bot.run(bot_token)
