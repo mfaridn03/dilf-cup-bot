10861009358dd7d8112a3ad145c015ba548931a3
Farid <mfaridn03@gmail.com>
2026-03-19 12:06:47 +1100

    refactor: offload processing to util file

--- osu.py
+++ osu.py
@@ -6,12 +6,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from data.store import RedisStore
+from utils import EmbedUtils
 
 load_dotenv()
 
 OSU_CLIENT_ID = os.environ.get("osu-client-id")
 OSU_CLIENT_SECRET = os.environ.get("osu-client-secret")
-GRADE_IMAGE_URL = "https://raw.githubusercontent.com/mfaridn03/dilf-cup-bot/refs/heads/main/data/grades/{}.png"
 
 class Osu(commands.Cog):
     def __init__(self, bot):
@@ -68,69 +68,13 @@ class Osu(commands.Cog):
         player_id = await self.bot.redis.get_discord_osu(ctx.author.id)
         if not player_id:
             return await ctx.send("You are not linked")
-        
-        recent_scores = await self.api.user_scores(
-            user_id=player_id,
-            type=ossapi.ScoreType.RECENT,
-            include_fails=False,
-            limit=1,
-            mode=ossapi.GameMode.OSU,
-            legacy_only=True,
+
+        embed = await EmbedUtils.recent_score(
+            ctx=ctx,
+            api=self.api,
+            redis=self.bot.redis,
+            player_id=player_id,
         )
-        if len(recent_scores) == 0:
-            return await ctx.send("No recent passes found")
-        
-        score = recent_scores[0]
-        artist = score.beatmapset.artist
-        title = score.beatmapset.title
-        diff = score.beatmap.version
-        pp = score.pp or 0
-        username = await self.bot.redis.get_osuname(ctx.author.id)
-
-        # data
-        acc = round(score.accuracy * 100, 2)
-        mods = [mod.acronym for mod in score.mods]
-        # get rid of CL unless nomod
-        if mods == ["CL"]:
-            mods = ["NM"]
-        else:
-            mods.remove("CL")
-
-        # hit counts
-        n0 = score.statistics.miss or 0
-        n50 = score.statistics.meh or 0
-        n100 = score.statistics.ok or 0
-        n300 = score.statistics.great or 0
-        hit_text = f"""```ansi
-[2;34m{n300}[0m[2;30m/[0m[2;36m{n100}[0m[2;30m/[0m[2;33m{n50}[0m[2;30m/[0m[2;31m{n0}[0m
-```"""
-        # max combo
-        play_combo = score.max_combo or 0
-        beatmap = await self.api.beatmap(beatmap_id=score.beatmap_id)
-        map_combo = beatmap.max_combo or -1
-
-        if map_combo == -1:
-            raise Exception(f"Beatmap max combo is None for beatmap ID: {score.beatmap_id}")
-
-        # set up embed
-        embed = discord.Embed()
-        embed.title = f"{artist} - {title} [{diff}]"
-        embed.url = score.beatmap.url
-        embed.set_image(url=score.beatmapset.covers.cover)
-        embed.set_footer(text=f"Mapset by {score.beatmapset.creator}")
-        embed.set_thumbnail(url=GRADE_IMAGE_URL.format(score.rank.value))
-        embed.set_author(
-            name=username,
-            url=f"https://osu.ppy.sh/users/{player_id}",
-            icon_url=ctx.author.display_avatar.url
-        )
-
-        # fields
-        embed.add_field(name="pp", value=f"{pp}pp")
-        embed.add_field(name="mods", value=", ".join(mods))
-        embed.add_field(name="acc", value=f"{acc}%")
-        embed.add_field(name="combo", value=f"**{play_combo}x**/{map_combo}x")
-        embed.add_field(name=" ", value=hit_text, inline=False)
         
         await ctx.send(embed=embed)
 
---
+++ utils.py
@@ -0,0 +1,69 @@
+import discord
+import ossapi
+
+from discord.ext import commands
+
+
+class EmbedUtils:
+    GRADE_IMAGE_URL = "https://raw.githubusercontent.com/mfaridn03/dilf-cup-bot/refs/heads/main/data/grades/{}.png"
+
+    @classmethod
+    async def recent_score(
+        cls,
+        ctx: commands.Context,
+        api: ossapi.OssapiAsync,
+        redis,
+        player_id: int,
+    ) -> discord.Embed:
+        recent_scores = await api.user_scores(
+            user_id=player_id,
+            type=ossapi.ScoreType.RECENT,
+            include_fails=False,
+            limit=1,
+            mode=ossapi.GameMode.OSU,
+            legacy_only=True,
+        )
+        if len(recent_scores) == 0:
+            raise ValueError("No recent passes found")
+
+        score = recent_scores[0]
+        pp = score.pp or 0
+        username = await redis.get_osuname(ctx.author.id)
+        acc = round(score.accuracy * 100, 2)
+        mods = [mod.acronym for mod in score.mods]
+        if mods == ["CL"]:
+            mods = ["NM"]
+        elif "CL" in mods:
+            mods.remove("CL")
+
+        n0 = score.statistics.miss or 0
+        n50 = score.statistics.meh or 0
+        n100 = score.statistics.ok or 0
+        n300 = score.statistics.great or 0
+        hit_text = f"""```ansi
+[2;34m{n300}[0m[2;30m/[0m[2;36m{n100}[0m[2;30m/[0m[2;33m{n50}[0m[2;30m/[0m[2;31m{n0}[0m
+```"""
+
+        play_combo = score.max_combo or 0
+        beatmap = await api.beatmap(beatmap_id=score.beatmap_id)
+        map_combo = beatmap.max_combo or -1
+        if map_combo == -1:
+            raise ValueError(f"Beatmap max combo is None for beatmap ID: {score.beatmap_id}")
+
+        embed = discord.Embed()
+        embed.title = f"{score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]"
+        embed.url = score.beatmap.url
+        embed.set_image(url=score.beatmapset.covers.cover)
+        embed.set_footer(text=f"Mapset by {score.beatmapset.creator}")
+        embed.set_thumbnail(url=cls.GRADE_IMAGE_URL.format(score.rank.value))
+        embed.set_author(
+            name=username,
+            url=f"https://osu.ppy.sh/users/{player_id}",
+            icon_url=ctx.author.display_avatar.url,
+        )
+        embed.add_field(name="pp", value=f"{pp}pp")
+        embed.add_field(name="mods", value=", ".join(mods))
+        embed.add_field(name="acc", value=f"{acc}%")
+        embed.add_field(name="combo", value=f"**{play_combo}x**/{map_combo}x")
+        embed.add_field(name=" ", value=hit_text, inline=False)
+        return embed
