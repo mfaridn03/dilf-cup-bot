4beecf2d9269d9ff36537e0615f82904d74b3797
Farid <mfaridn03@gmail.com>
2026-03-19 11:11:21 +1100

    feat: recent score combo

--- osu.py
+++ osu.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 OSU_CLIENT_ID = os.environ.get("osu-client-id")
 OSU_CLIENT_SECRET = os.environ.get("osu-client-secret")
+GRADE_IMAGE_URL = "https://raw.githubusercontent.com/mfaridn03/dilf-cup-bot/refs/heads/main/data/grades/{}.png"
 
 class Osu(commands.Cog):
     def __init__(self, bot):
@@ -17,7 +18,7 @@ class Osu(commands.Cog):
             client_id=OSU_CLIENT_ID,
             client_secret=OSU_CLIENT_SECRET,
         )
-
+    
     @commands.is_owner()
     @commands.command(name="set")
     async def cmd_set(self, ctx: commands.Context, member: discord.Member = None, username: str = None):
@@ -74,16 +75,33 @@ class Osu(commands.Cog):
         artist = score.beatmapset.artist
         title = score.beatmapset.title
         diff = score.beatmap.version
-        pp = score.pp
+        pp = score.pp or 0
 
         # data
         acc = round(score.accuracy * 100, 2)
         mods = [mod.acronym for mod in score.mods]
         # get rid of CL unless nomod
         if mods == ["CL"]:
-            mods = "NM"
+            mods = ["NM"]
         else:
             mods.remove("CL")
+
+        # hit counts
+        n0 = score.statistics.miss or 0
+        n50 = score.statistics.meh or 0
+        n100 = score.statistics.ok or 0
+        n300 = score.statistics.great or 0
+        hit_text = f"""```ansi
+[2;34m{n300}[0m/[2;36m{n100}[0m/[2;33m{n50}[0m/[2;31m{n0}[0m
+```
+"""
+        # max combo
+        play_combo = score.max_combo or 0
+        beatmap = await self.api.beatmap(beatmap_id=score.beatmap_id)
+        map_combo = beatmap.max_combo or -1
+
+        if map_combo == -1:
+            raise Exception(f"Beatmap max combo is None for beatmap ID: {score.beatmap_id}")
 
         # set up embed
         embed = discord.Embed()
@@ -91,11 +109,14 @@ class Osu(commands.Cog):
         embed.url = score.beatmap.url
         embed.set_image(url=score.beatmapset.covers.cover)
         embed.set_footer(text=f"Mapset by {score.beatmapset.creator}")
+        embed.set_thumbnail(url=GRADE_IMAGE_URL.format(score.rank.value))
 
         # fields
         embed.add_field(name="pp", value=f"{pp}pp")
+        embed.add_field(name="mods", value=", ".join(mods))
         embed.add_field(name="acc", value=f"{acc}%")
-        embed.add_field(name="mods", value=", ".join(mods), inline=False)
+        embed.add_field(name="combo", value=f"**{play_combo}x**/{map_combo}x")
+        embed.add_field(name=" ", value=hit_text, inline=False)
         
         await ctx.send(embed=embed)
 
